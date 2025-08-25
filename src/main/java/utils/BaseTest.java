9f39917e5255596ebcb0493ac703a826cbeff38e
Matas Backevicius <84940755+Mugdass@users.noreply.github.com>
2025-08-25 10:54:24 -0700

    Update BaseTest.java

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -1,1 +1,16 @@
+package utils;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    protected RequestSpecification request;
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://reqres.in/api";
+        request = RestAssured.given().header("Content-Type", "application/json");
+    }
+}
 
