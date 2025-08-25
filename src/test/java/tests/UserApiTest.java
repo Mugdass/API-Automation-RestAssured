4269c2f401c3e3feba52e04dba3de72f4fdfa6ab
Matas Backevicius <84940755+Mugdass@users.noreply.github.com>
2025-08-25 10:57:36 -0700

    Update UserApiTest.java

--- src/test/java/tests/UserApiTest.java
+++ src/test/java/tests/UserApiTest.java
@@ -1,1 +1,42 @@
+package tests;
+
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.BaseTest;
+
+public class UserApiTest extends BaseTest {
+
+    @Test(priority = 1)
+    public void getUserList_ShouldReturn200() {
+        Response response = request
+                .when()
+                .get("/users?page=2")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertTrue(response.jsonPath().getList("data").size() > 0);
+    }
+
+    @Test(priority = 2)
+    public void createUser_ShouldReturn201() {
+        String payload = """
+                {
+                  "name": "John",
+                  "job": "QA Engineer"
+                }
+                """;
+
+        Response response = request
+                .body(payload)
+                .when()
+                .post("/users")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(response.getStatusCode(), 201);
+        Assert.assertEquals(response.jsonPath().getString("name"), "John");
+    }
+}
 
